22a5d6039382976468a9ef587c728bfe1a220370
Teri Radichel <nunya>
2023-02-17 18:42:07 +0000

    untested SCP and OU code

---
+++ Org/stacks/SCP/scp_functions.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -e
+# https://github.com/tradichel/SecurityMetricsAutomation/
+# Org/stacks/IdP/idp_functions.sh
+# author: @teriradichel @2ndsightlab
+# description: Functions for idp creation
+##############################################################
+source ../../../Functions/shared_functions.sh
+
+profile="Governance"
+
+deploy_scp(){
+	name=$1
+	targetids=$2
+
+  func=${FUNCNAME[0]}
+  validate_param 'scpname' $name $func
+  validate_param 'targetids' $targetids $func
+	
+  parameters=$(add_parameter "NameParam" $name)
+  parameters=$(add_parameter "TargetIdsParam" $targetids $parameters)
+
+  resourcetype='SCP'
+  template='cfn/$name.yaml'
+  deploy_stack $profile $name $resourcetype $template $parameters
+}
+
+################################################################################
+# Copyright Notice
+# All Rights Reserved.
+# All materials (the “Materials”) in this repository are protected by copyright 
+# under U.S. Copyright laws and are the property of 2nd Sight Lab. They are provided 
+# pursuant to a royalty free, perpetual license the person to whom they were presented 
+# by 2nd Sight Lab and are solely for the training and education by 2nd Sight Lab.
+#
+# The Materials may not be copied, reproduced, distributed, offered for sale, published, 
+# displayed, performed, modified, used to create derivative works, transmitted to 
+# others, or used or exploited in any way, including, in whole or in part, as training 
+# materials by or for any third party.
+#
+# The above copyright notice and this permission notice shall be included in all 
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+################################################################################  
