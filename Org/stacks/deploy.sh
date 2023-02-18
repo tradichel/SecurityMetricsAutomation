cb1cebc6404d72aafdaf86797688bf8e6fac3f01
Teri Radichel <nunya>
2023-02-18 05:19:03 +0000

    Messages in Org deploy script

--- Org/stacks/deploy.sh
+++ Org/stacks/deploy.sh
@@ -1,11 +1,19 @@
+#!/bin/bash -e
+# https://github.com/tradichel/SecurityMetricsAutomation/
+# Org/stacks/deploy.sh
+# author: @teriradichel @2ndsightlab
+# description: Deploy AWS IAM IdP
+##############################################################
 
-
+echo "Have you deployed the OrgRoot user in /Org/stacsk/OrgRoot/depoy.sh? Enter to continue. Ctrl-C to exit."
+read ok
 cd Organization
 ./deploy.sh
 cd ../KMS
 ./deploy.sh
 cd ../Secrets
 ./deploy.sh
+
 echo "Add the metadata for Okta to the new secret: OrgRootSecrets. Enter to continue. Ctrl-C to exit"
 read ok
 cd ../IdP
